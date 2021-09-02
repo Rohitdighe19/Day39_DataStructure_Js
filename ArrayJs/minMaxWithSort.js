7bcc61d10f5f7116f09fe3a713ee9a73b134b42a
rohitdighe19 <rohitdighe47@gmail.com>
2021-09-02 12:04:54 +0530

    Added:Solution Data Struture Program uning js

---
+++ ArrayJs/minMaxWithSort.js
@@ -0,0 +1,17 @@
+/**
+ * Problem : Extend the above program to sort the array and then find the 2nd largest 
+ *           and the 2nd smallest element.
+ */
+
+function minMaxWithSort(){
+    let arrayOfRandomNumbers = [];
+    for(i = 0; i < 10; i++){
+        randomNumber = Math.floor(Math.random() * 899 + 100);
+        arrayOfRandomNumbers.push(randomNumber);
+    }
+    arrayOfRandomNumbers.sort();
+    console.log(`10 Random Numbers in sorted order are : ${arrayOfRandomNumbers}`);
+    console.log(`Second largest element is : ${arrayOfRandomNumbers[8]}`);
+    console.log(`Second smallest element is : ${arrayOfRandomNumbers[1]}`);
+}
+minMaxWithSort();
